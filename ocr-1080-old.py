72a8c622a5600ba833df6d0eb6b8753c46092a83
iCallH4x <64992884+iCallH4x@users.noreply.github.com>
2022-10-13 12:11:18 +0100

    Add files via upload

---
+++ ocr-1080-old.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+import pytesseract
+import pyperclip
+
+from PIL import ImageGrab
+
+# U need Python
+# And Some libaries
+# pip install opencv-python
+# pip install numpy
+# pip install Pillow
+# pip install pyperclip
+
+# pip install pytesseract
+# also read https://pypi.org/project/pytesseract/
+
+
+while True:
+
+    #Perfect in Twitches Cinema-Mode
+    cap = ImageGrab.grab(bbox=(1020, 950, 1550, 992))
+    cap_arr = np.array(cap)
+
+
+    cv2.imshow("", cap)
+
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    text = pytesseract.image_to_string(cap)
+
+
+    text = text.strip()
+
+
+    if len(text) > 20:
+        print(text)
+        pyperclip.copy(text)
+
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cv2.destroyAllWindows()
---
+++ ocr-1440-old.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import pytesseract
+import pyperclip
+
+from PIL import ImageGrab
+
+# U need Python
+# And Some libaries
+# pip install opencv-python
+# pip install numpy
+# pip install Pillow
+# pip install pyperclip
+
+# pip install pytesseract
+# also read https://pypi.org/project/pytesseract/
+
+
+while True:
+
+    #Perfect in Twitches Cinema-Mode
+    cap = ImageGrab.grab(bbox=(1370, 1224, 2099, 1281))
+    cap_arr = np.array(cap)
+
+
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+    text = pytesseract.image_to_string(cap)
+
+
+    text = text.strip()
+
+
+    if len(text) > 20:
+        print(text)
+        pyperclip.copy(text)
+
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cv2.destroyAllWindows()
